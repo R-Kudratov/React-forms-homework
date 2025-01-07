898c6c71e3a1357112f8ecf5fd6545b149cd12cf
Rustam Kudratov <kudratov.rustik@gmail.com>
2025-01-07 04:08:56 +0300

    functionality implemented using React-hook-form and yup

--- src/components/registration.jsx
+++ src/components/registration.jsx
@@ -1,154 +1,101 @@
-import { useRef, useState } from 'react'
-import propTypes from 'prop-types'
+import { useEffect, useRef } from 'react'
+import { useForm } from 'react-hook-form'
+import * as yup from 'yup'
+import { yupResolver } from '@hookform/resolvers/yup'
 import styles from './registration.module.css'
 
+const validationSchema = yup.object().shape({
+	email: yup
+		.string()
+		.required('Введите email')
+		.matches(
+			/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
+			'Введен некорректный email',
+		),
+	password: yup
+		.string()
+		.required('Введите пароль')
+		.min(8, 'Пароль должен содержать минимум 8 символов')
+		.matches(
+			/[A-Z]/,
+			'Пароль должен содержать хотя бы одну заглавную букву',
+		)
+		.matches(/[a-z]/, 'Пароль должен содержать хотя бы одну строчную букву')
+		.matches(/\d/, 'Пароль должен содержать хотя бы одну цифру'),
+	passwordConfirmation: yup
+		.string()
+		.required('Подтвердите пароль')
+		.oneOf([yup.ref('password')], 'Пароли не совпадают'),
+})
+
 const RegistrationLayout = ({
-	validateEmail,
-	validatePassword,
-	validatePasswordConfirmation,
+	register,
+	errors,
 	onSubmit,
 	submitButtonRef,
-	displayErrors,
 }) => {
 	return (
 		<form className={styles.registration} onSubmit={onSubmit}>
-			{displayErrors && <p className={styles.error}>{displayErrors}</p>}
-			<input
-				type="email"
-				placeholder="email"
-				onBlur={({ target }) => {
-					validateEmail(target.value)
-				}}
-			/>
+			{errors && <p className={styles.error}>{errors}</p>}
+			<input type="email" placeholder="email" {...register('email')} />
 			<input
 				type="password"
 				placeholder="пароль"
-				onBlur={({ target }) => validatePassword(target.value)}
+				{...register('password')}
 			/>
 			<input
 				type="password"
 				placeholder="подтвердите пароль"
-				onChange={({ target }) => {
-					validatePasswordConfirmation(target.value)
-				}}
+				{...register('passwordConfirmation')}
 			/>
-			<button
-				type="submit"
-				ref={submitButtonRef}
-				disabled={displayErrors}
-			>
+			<button type="submit" ref={submitButtonRef} disabled={!!errors}>
 				Зарегистрироваться
 			</button>
 		</form>
 	)
 }
 
-RegistrationLayout.propTypes = {
-	displayErrors: propTypes.string,
-	validateEmail: propTypes.func.isRequired,
-	validatePassword: propTypes.func.isRequired,
-	onSubmit: propTypes.func.isRequired,
-	validatePasswordConfirmation: propTypes.func.isRequired,
-	submitButtonRef: propTypes.object.isRequired,
-}
-
 export const Registration = () => {
-	const [email, setEmail] = useState('')
-	const [password, setPassword] = useState('')
-	const [passwordConfirmation, setPasswordConfirmation] = useState('')
-	const [errors, setErrors] = useState({})
+	const {
+		register,
+		handleSubmit,
+		formState: { errors, isValid },
+		reset,
+	} = useForm({
+		mode: 'onBlur',
+		defaultValues: { email: '', password: '', passwordConfirmation: '' },
+		resolver: yupResolver(validationSchema),
+	})
+
+	const getErrorsText = () => {
+		const errorsText = [
+			errors.email?.message,
+			errors.password?.message,
+			errors.passwordConfirmation?.message,
+		]
+		return errorsText.filter(Boolean).join('\n')
+	}
 
 	const submitButtonRef = useRef(null)
 
-	const emailRegex = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/
-	const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d).{8,}$/
-
-	const validateEmail = (email) => {
-		const newError = { ...errors, email: null }
-
-		if (!email) {
-			newError.email = 'Введите email'
-		} else if (!emailRegex.test(email)) {
-			newError.email = 'Введен некорректный email'
-		} else {
-			newError.email = null
-			setEmail(email)
-		}
-
-		setErrors(newError)
-	}
-
-	const validatePassword = (password) => {
-		const newError = { ...errors, password: null }
-
-		if (!password) {
-			newError.password = 'Введите пароль'
-		} else if (!passwordRegex.test(password)) {
-			newError.password =
-				'Пароль должен содержать минимум 8 символов, хотя бы одну заглавную букву, одну строчную букву и одну цифру'
-		} else {
-			newError.password = null
-			setPassword(password)
-		}
-
-		setErrors(newError)
-	}
-
-	const validatePasswordConfirmation = (repeatedPassword) => {
-		const newError = { ...errors, repeatedPassword: null }
-
-		if (!repeatedPassword) {
-			newError.repeatedPassword = 'Подтвердите пароль'
-		} else if (repeatedPassword !== password) {
-			newError.repeatedPassword = 'Пароли не совпадают'
-		} else if (repeatedPassword === password) {
-			setPasswordConfirmation(repeatedPassword)
-			newError.repeatedPassword = null
+	useEffect(() => {
+		if (isValid) {
 			submitButtonRef.current.disabled = false
 			submitButtonRef.current.focus()
 		}
+	}, [isValid])
 
-		setErrors(newError)
-	}
-
-	const displayErrors = () => {
-		return Object.values(errors).filter(Boolean).join('\n')
-	}
-
-	const validateForm = () => {
-		const newErrors = {}
-
-		if (!email) newErrors.email = 'Введите email'
-		if (!password) newErrors.password = 'Введите пароль'
-		if (!passwordConfirmation)
-			newErrors.repeatedPassword = 'Подтвердите пароль'
-
-		setErrors(newErrors)
-
-		return Object.keys(newErrors).length === 0
-	}
-
-	const onSubmit = (event) => {
-		event.preventDefault()
-
-		if (validateForm()) {
-			console.log(email, password)
-			event.target.reset()
-			setEmail('')
-			setPassword('')
-			setPasswordConfirmation('')
-			setErrors({})
-		}
+	const onSubmit = (data) => {
+		console.log(data)
+		reset()
 	}
 
 	return (
 		<RegistrationLayout
-			validateEmail={validateEmail}
-			validatePassword={validatePassword}
-			validatePasswordConfirmation={validatePasswordConfirmation}
-			onSubmit={onSubmit}
+			register={register}
+			errors={getErrorsText()}
+			onSubmit={handleSubmit(onSubmit)}
 			submitButtonRef={submitButtonRef}
-			displayErrors={displayErrors()}
 		/>
 	)
 }
